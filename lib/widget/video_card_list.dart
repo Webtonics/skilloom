1d4031119f9a2d5915f94040f8b0394124e68a8c
Emmanuel Uchenna <Camplodgeinc@gmail.com>
2023-10-17 23:00:03 -0700

    fixed bugs

--- lib/widget/video_card_list.dart
+++ lib/widget/video_card_list.dart
@@ -12,12 +12,19 @@ class LessonList extends StatelessWidget {
           height: 70,
           padding: const EdgeInsets.symmetric(horizontal: 8),
           decoration: const BoxDecoration(
+            // border: Bo,
+            borderRadius: BorderRadius.all(Radius.circular(21)),
+            border: Border.symmetric(
+                vertical: BorderSide(
+                  color: Colors.black,
+                ),
+                horizontal: BorderSide(color: Colors.black)),
             color: Colors.white,
           ),
           child: ListTile(
             leading: const Icon(
               Icons.play_circle,
-              size: 50,
+              size: 30,
             ),
             title: Text("Title $index"),
             subtitle: Text("This is the description of $index"),
