ef114f5280175817b214b180adf1e9e08d7b80c7
Emmanuel Uchenna <Camplodgeinc@gmail.com>
2023-10-21 06:43:21 -0700

    lesson integration

--- lib/views/app_screen/course_lesson.dart
+++ lib/views/app_screen/course_lesson.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
-class CourseLesson extends StatefulWidget {
-  const CourseLesson({super.key});
+import '../../constants/spacing.dart';
+import '../../constants/theme/theme.dart';
+import '../../utils/elevated_buttons.dart';
+import '../../widget/video_card_list.dart';
 
+class CourseLesson extends StatefulWidget {
+  const CourseLesson({
+    super.key,
+    this.snap,
+  });
+  final snap;
   @override
   State<CourseLesson> createState() => _CourseLessonState();
 }
@@ -10,9 +18,68 @@ class CourseLesson extends StatefulWidget {
 class _CourseLessonState extends State<CourseLesson> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("CourseLesson"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("widget.snap['title']"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Container(
+                height: 250,
+                width: double.infinity,
+                decoration: const BoxDecoration(color: Colors.deepPurple),
+                child: const Center(
+                  child: Text("Video"),
+                ),
+              ),
+
+              spacingH,
+              const Center(
+                child: Text(
+                  "${"widget.courseName"} information",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                ),
+              ),
+              spacingH,
+              const Center(
+                child: Text("${"widget.snap['description']"} "),
+              ),
+              spacingH,
+              MyElevattedButton(
+                  title: "Full Note",
+                  action: () {
+                    // LinkService().launchURL(widget.snap['fullNote'], context);
+                  }),
+              spacingH,
+              const Text(
+                "All Lessons",
+                style: headingSmall,
+              ),
+              spacingH,
+              // Padding(
+              //   padding: const EdgeInsets.all(8.0),
+              //   child: Container(
+              //       height: double.maxFinite,
+              //       child: ListView.builder(
+              //           itemCount: 10,
+              //           itemBuilder: ((context, index) {
+              //             return LessonList(
+              //               index: index,
+              //             );
+              //           }))),
+              // ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // LinkService().launchURL(widget.snap['groupChat'], context);
+        },
+        child: const Icon(Icons.chat),
       ),
     );
   }
--- lib/widget/video_card_list.dart
+++ lib/widget/video_card_list.dart
@@ -1,34 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:skilloom/views/app_screen/course_lesson.dart';
 
 class LessonList extends StatelessWidget {
-  const LessonList({super.key, required this.index});
+  const LessonList({
+    super.key,
+    required this.index,
+  });
   final int index;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-          height: 70,
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          decoration: const BoxDecoration(
-            // border: Bo,
-            borderRadius: BorderRadius.all(Radius.circular(21)),
-            border: Border.symmetric(
-                vertical: BorderSide(
-                  color: Colors.black,
-                ),
-                horizontal: BorderSide(color: Colors.black)),
-            color: Colors.white,
-          ),
-          child: ListTile(
-            leading: const Icon(
-              Icons.play_circle,
-              size: 40,
+      child: GestureDetector(
+        onTap: () => Navigator.of(context).push(
+          MaterialPageRoute(builder: ((context) => const CourseLesson())),
+        ),
+        child: Container(
+            height: 70,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            decoration: const BoxDecoration(
+              // border: Bo,
+              borderRadius: BorderRadius.all(Radius.circular(21)),
+              border: Border.symmetric(
+                  vertical: BorderSide(
+                    color: Colors.black,
+                  ),
+                  horizontal: BorderSide(color: Colors.black)),
+              color: Colors.white,
             ),
-            title: Text("Title $index"),
-            subtitle: Text("This is the description of $index"),
-          )),
+            child: ListTile(
+              leading: const Icon(
+                Icons.play_circle,
+                size: 40,
+              ),
+              title: Text("Title $index"),
+              subtitle: Text("This is the description of $index"),
+            )),
+      ),
     );
   }
 }
