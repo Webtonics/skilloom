71d362b45a429d3f082dcb0a89c0bb05f9391fcf
webtonics <camplodgeinc@gmail.com>
2023-12-15 11:21:07 -0800

    new update

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,20 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:skilloom/Services/authservice/user_services.dart';
-
-// import '../models/user_model.dart';
-
-// class UserProvider with ChangeNotifier {
-//   User? _user;
-
-//   final AuthService _authService = AuthService();
-//   User? get getUser => _user;
-
-//   Future<void> refreshUser() async {
-//     User? user = await _authService.getUserDetails();
-//     _user = user;
-//     notifyListeners();
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:skilloom/Services/authservice/user_services.dart';
 import '../models/user_model.dart';
